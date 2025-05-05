0f9fee354dffed574c0fff4ee0674f7b77382de9
biaaPrado <104280744+biaaPrado@users.noreply.github.com>
2025-05-05 16:06:29 -0300

    alguma mudança

--- Alocação Dinamica e Struct/ex1.c
+++ Alocação Dinamica e Struct/ex1.c
@@ -7,18 +7,6 @@ typedef struct{
     float* precos; //vetor dos preços dos livros desejados
 }Cliente;
 
-int ordena(const void* p0, const void* p1){
-    float fp0 = *(float*)p0;
-    float fp1 = *(float*)p1;
-    if (fp0 < fp1) 
-        return -1;
-    else if(fp0 > fp1)
-        return 1;
-    else
-        return 0;
-}
-
-
 void lerDados(Cliente *clientes, int n){ //recebe a struct como paramêtro e a qntd de clientes
     int i, j;
 
@@ -26,31 +14,24 @@ void lerDados(Cliente *clientes, int n){ //recebe a struct como paramêtro e a q
         float D;
         int Q;
 
-        scanf("%f %d", &D, &Q); //lê o saldo e qntd de livros, fazendo as devidas verificações
+        do{
+            scanf("%f", &D); //lê qntd de livros
+        }while(D < 0);
 
-        if(D < 0 || Q < 0 || Q > 100){
-            exit(0);
-        }
+        do{
+            scanf("%d", &Q); //lê o saldo
+        }while(Q < 0 || Q > 100);
 
         clientes[i].dinheiro = D;
         clientes[i].qtde = Q;
 
         clientes[i].precos = (float*) malloc(Q * sizeof(float)); //aloca espaços para os preços
-        if(clientes[i].precos == NULL){
-            exit(0);
-        }
-
-        float* P = clientes[i].precos; 
-    
-
+            
         for (j=0; j<Q; j++){ //recebe os precos dos livros escolhidos pelo cliente
-            scanf("%f", &P[j]); 
-            if(P[j]<0){
-                exit(0);
-            }
+            do{
+                scanf("%f", &clientes[i].precos[j]);
+            }while(clientes[i].precos[j]<0);
         }
-
-        qsort(P, Q, sizeof(float), ordena); //ordena os preços em ordem crescente
     }
 }
 
@@ -61,33 +42,24 @@ void calculaGasto(Cliente *clientes, int n){
         int livrosComprados = 0;
         int Q = clientes[i].qtde;
         float saldoCliente = clientes[i].dinheiro;
-        float* P = clientes[i].precos;
 
         for(j=0; j<Q; j++){
-           if(saldoCliente >= P[j]){
-                saldoCliente -= P[j];
+           if(saldoCliente >= clientes[i].precos[j]){
+                saldoCliente -= clientes[i].precos[j];
                 livrosComprados++;
-           }else{
-                break;
            }
         }
 
         printf("%d %.2f\n", livrosComprados, saldoCliente);
-
-        free(P); //libera o espaço de memória alocado pelo vetor de preços
     }
 }
 
 int main(){
     int n;
-    scanf("%d", &n);
-
-    if(n<=0 || n>100){ //verifica se a quantidade de clientes é válida no intervalo
-        return 0;
-    }
-    if(n == 0){
-        return 0;
-    }
+    
+    do{
+        scanf("%d", &n);
+    }while(n<1 || n>100);
 
     Cliente *clientes = (Cliente*) malloc(n * sizeof(Cliente)); //aloca espaço para a struct clientes
 
@@ -97,7 +69,11 @@ int main(){
 
     lerDados(clientes, n);
     calculaGasto(clientes, n);
+    int i;
 
+    for(i=0; i<n; i++){
+        free(clientes[i].precos);
+    }
     free(clientes); //libera o espaço de memória alocado para os clientes
 
     return 0;
