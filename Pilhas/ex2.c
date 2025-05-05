2418d5cca9f0f5e7eb38b04b2703d7e8d969cdd6
biaaPrado <104280744+biaaPrado@users.noreply.github.com>
2025-05-05 17:01:19 -0300

    mais certo - score 70

--- Pilhas/ex2.c
+++ Pilhas/ex2.c
@@ -31,7 +31,7 @@ void push(Pilha* p, int valor){
 int pop(Pilha* p){
     if(p->topo == NULL){
         printf("Erro: Pilha vazia!\n");
-        exit(1);
+        exit(0);
     }
     No* temp = p->topo;
     int valor = temp->valor;
@@ -42,22 +42,29 @@ int pop(Pilha* p){
 }
  
 void printPilha(Pilha* p){
+    if(p->tam == 0){
+        printf("\n");
+        return;
+    }
+    
     No* atual = p->topo;
     int* valores = malloc(p->tam * sizeof(int));
-    int i = 0;
+    if(valores == NULL){
+        exit(1);
+    }
  
+    int i = 0;
     while(atual != NULL){
         valores[i++] = atual->valor;
         atual = atual->prox;
     }
  
-    int j;
+    int j=0;
     for(j=0; j<p->tam; j++){
         printf("%d", valores[j]);
         if(j<p->tam-1)
             printf(" ");
     }
- 
     printf("\n");
     free(valores);
 }
@@ -70,8 +77,8 @@ void clearPilha(Pilha* p){
  
 void add(Pilha* p){
     if(p->tam < 2){
-        printf("Erro: Elementos insuficientes para operação!\n");
-        exit(EXIT_FAILURE);
+        printf("Erro: Faltam elementos na pilha!\n");
+        exit(0);
     }
     int a = pop(p);
     int b = pop(p);
@@ -80,8 +87,8 @@ void add(Pilha* p){
  
 void sub(Pilha* p){
     if(p->tam < 2){
-        printf("Erro: Elementos insuficientes para operação!\n");
-        exit(EXIT_FAILURE);
+        printf("Erro: Faltam elementos na pilha!\n");        
+        exit(0);
     }
     int a = pop(p);
     int b = pop(p);
@@ -90,8 +97,8 @@ void sub(Pilha* p){
  
 void mult(Pilha* p){
     if(p->tam < 2){
-        printf("Erro: Elementos insuficientes para operação!\n");
-        exit(EXIT_FAILURE);
+        printf("Erro: Faltam elementos na pilha!\n");
+        exit(0);
     }
     int a = pop(p);
     int b = pop(p);
@@ -100,13 +107,13 @@ void mult(Pilha* p){
  
 void divisao(Pilha* p){
     if(p->tam < 2){
-        printf("Erro: Elementos insuficientes para operacao!\n");
+        printf("Erro: Faltam elementos na pilha!\n");
         exit(EXIT_FAILURE);
     }
     int a = pop(p);
-    if(a==0){
+    if(a == 0){
         printf("Erro: Divisao por zero!\n");
-        exit(EXIT_FAILURE);
+        exit(0);
     }
     int b = pop(p);
     push(p, b / a);
@@ -115,17 +122,16 @@ void divisao(Pilha* p){
 void dup(Pilha* p){
     if(p->tam < 1){
         printf("Erro: Pilha vazia para DUP!\n");
-        exit(EXIT_FAILURE);
+        exit(0);
     }
-    int a = pop(p);
-    push(p, a);
+    int a = p->topo->valor;
     push(p, a);
 }
  
 void swap(Pilha* p){
     if(p->tam < 2){
         printf("Erro: Pilha insuficiente para SWAP!\n");
-        exit(EXIT_FAILURE);
+        exit(0);
     }
     int a = pop(p);
     int b = pop(p);
@@ -135,25 +141,23 @@ void swap(Pilha* p){
  
 void roll(Pilha* p, int n){
     if(n <= 0 || n > p->tam){
-        printf("Erro: n fora dos limites!\n");
-        exit(EXIT_FAILURE);
+        printf("Erro: Posicao invalida!\n");
+        exit(0);
     }
  
-    Pilha temp; 
-    inicializa(&temp);
+    Pilha aux;
+    inicializa(&aux);
  
     int i;
-    for(i=1; i<n; i++){
-        push(&temp, pop(p));
+    for (i = 0; i < n-1; i++) {
+        push(&aux, pop(p));
     }
  
-    int elemento = pop(p);
- 
-    while(temp.topo !=  NULL){
-        push(p, pop(&temp));
+    int elemento_n = pop(p); // o n-ésimo elemento
+    while (aux.tam > 0) {
+        push(p, pop(&aux));
     }
- 
-    push(p, elemento);
+    push(p, elemento_n);
 }
  
  
@@ -161,11 +165,13 @@ int main(){
     Pilha p;
     inicializa(&p);
  
-    int N;
-    scanf("%d", &N);
+    int N, ch;
+    if (scanf("%d", &N) != 1 || N < 1 || N > 100) {
+        printf("Erro: n fora dos limites!\n");
+        return 0;
+    }
  
-    int c;
-    while((c = getchar()) != '\n' && c != EOF);
+    do { ch = getchar(); } while (ch != '\n' && ch != EOF);
  
     int i;
     for (i=0; i<N; i++){
@@ -184,15 +190,13 @@ int main(){
         else if(strcmp(comando, "POP") == 0){pop(&p);}
         else if(strcmp(comando, "ADD") == 0){add(&p);}        
         else if(strcmp(comando, "SUB") == 0){sub(&p);}
-        else if(strcmp(comando, "MULT") == 0){mult(&p);}
+        else if(strcmp(comando, "MUL") == 0){mult(&p);}
         else if(strcmp(comando, "DIV") == 0){divisao(&p);}
         else if(strcmp(comando, "DUP") == 0){dup(&p);}
         else if(strcmp(comando, "SWAP") == 0){swap(&p);}
         else if(strcmp(comando, "ROLL") == 0){
             int pos; 
             if(scanf("%d", &pos) != 1){
-                printf("Erro: n fora dos limites!\n");
-                clearPilha(&p);
                 return 1;
             }
             roll(&p, pos);
@@ -200,7 +204,9 @@ int main(){
         else if(strcmp(comando, "CLEAR") == 0){clearPilha(&p);}
         else if(strcmp(comando, "PRINT") == 0){printPilha(&p);}
  
-        while(getchar() != '\n');
+        do {
+            ch = getchar();
+        } while (ch != '\n' && ch != EOF);
     } 
  
     return 0;
